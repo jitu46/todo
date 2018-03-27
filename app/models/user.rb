c7002f219bfff5a2571b2ac0a482748f5bbcc54a
jitender ojha <jitender.mca.du.2015@gmail.com>
2018-03-27 11:29:06 +0530

    username added

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
 
+  def self.find_for_database_authentication warden_conditions
+  	conditions = warden_conditions.dup
+  	login = conditions.delete(:login)
+  	where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
 
 end
